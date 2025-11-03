d7fa81e061dd4b4142bf4bf3021760caf3127509
Jackson Newman <jpnewman167@gmail.com>
2025-11-03 14:37:58 -0800

    fixed exports

--- github_action_scripts/cik_lookup_table/utils/__init__.py
+++ github_action_scripts/cik_lookup_table/utils/__init__.py
@@ -5,15 +5,15 @@ Utils package for CIK lookup table synchronization.
 from .utils import (
     fetch_ticker_data_from_github_repo,
     lookup_cik_and_company_name_batch,
-    process_tickers_in_batches,
-    create_cik_lookup_entities,
-    analyze_synchronization_operations,
+    process_tickers_and_persist_ciks,
+    identify_ciks_to_delete,
+    delete_obsolete_ciks,
 )
 
 __all__ = [
     'fetch_ticker_data_from_github_repo',
     'lookup_cik_and_company_name_batch',
-    'process_tickers_in_batches',
-    'create_cik_lookup_entities',
-    'analyze_synchronization_operations',
+    'process_tickers_and_persist_ciks',
+    'identify_ciks_to_delete',
+    'delete_obsolete_ciks',
 ]
