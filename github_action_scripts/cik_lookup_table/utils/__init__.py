bb654b9376aedacc2ffba6a994f993ecbca10dec
Jackson Newman <jpnewman167@gmail.com>
2025-11-18 16:14:56 -0800

    fixed init for cik lookup utils

--- github_action_scripts/cik_lookup_table/utils/__init__.py
+++ github_action_scripts/cik_lookup_table/utils/__init__.py
@@ -5,13 +5,9 @@ Utils package for CIK lookup table synchronization.
 from .utils import (
     lookup_cik_and_company_name_batch,
     process_tickers_and_persist_ciks,
-    identify_ciks_to_delete,
-    delete_obsolete_ciks,
 )
 
 __all__ = [
     'lookup_cik_and_company_name_batch',
-    'process_tickers_and_persist_ciks',
-    'identify_ciks_to_delete',
-    'delete_obsolete_ciks',
+    'process_tickers_and_persist_ciks'
 ]
