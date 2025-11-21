079019a5c9d4b10f61344674624eb84612af4f66
Jackson Newman <jpnewman167@gmail.com>
2025-11-20 18:12:43 -0800

    Increased cooldown between batches

--- github_action_scripts/ticker_overview_table/utils/utils.py
+++ github_action_scripts/ticker_overview_table/utils/utils.py
@@ -270,7 +270,7 @@ def process_tickers_and_persist_overviews(
         
         logger.info(f"Processing batch {batch_num}/{total_batches} ({len(batch)} tickers)...")
         logger.info(f"Waiting between batches to avoid rate limiting...")
-        time.sleep(7)
+        time.sleep(8)
         
         # Lookup ticker overview data
         batch_results, yahoo_failed = get_ticker_overview_data_batch_from_yahoo_query(batch, session=session)
